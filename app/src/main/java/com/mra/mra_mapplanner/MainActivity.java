f19c8efd35b2e377206513bc169bb3e638ba21d3
mrizwanasim <phone.asim@gmail.com>
2020-03-22 18:05:55 +1100

    06 Android Google Maps Example

--- app/src/main/java/com/mra/mra_mapplanner/MainActivity.java
+++ app/src/main/java/com/mra/mra_mapplanner/MainActivity.java
@@ -3,13 +3,28 @@ package com.mra.mra_mapplanner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-//AsimTesting Branch
-//Merge Testing
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //seattle coordinates
+        LatLng seattle = new LatLng(47.6062095, -122.3320708);
+        mMap.addMarker(new MarkerOptions().position(seattle).title("Seattle"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(seattle));
     }
 }
